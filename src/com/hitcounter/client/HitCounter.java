82f9efac67640c408163e57fb3db3d1f636d699f
GameDeveIoper <petroffart3mka@yandex.ru>
2020-06-28 21:09:37 +0800

    Init 2

--- src/com/hitcounter/client/HitCounter.java
+++ src/com/hitcounter/client/HitCounter.java
@@ -1,5 +1,28 @@
 package com.hitcounter.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class HitCounter {
+    private static final int PORT_NUMBER = 63457;
+    private Socket socket;
+
+
+    public static void main(String[] args) {
+        new HitCounter();
+    }
+
+    public HitCounter(){
+        try{
+            socket = new Socket("127.0.0.1", PORT_NUMBER);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String s = in.readLine();
+            System.out.println("New visitor " + s);
+        }catch (IOException e){
+
+        }
+    }
 
 }
--- src/com/hitcounter/server/HitCounterServer.java
+++ src/com/hitcounter/server/HitCounterServer.java
@@ -1,9 +1,31 @@
 package com.hitcounter.server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HitCounterServer {
+    private static final int PORT_NUMBER = 63457;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private int count = 0;
+
+
 
     public HitCounterServer(){
+        System.out.println("Server is running...");
+        try{
+            serverSocket = new ServerSocket(PORT_NUMBER);
+            socket = serverSocket.accept();
+            System.out.println("New connections");
+            PrintWriter toCilent = new PrintWriter(socket.getOutputStream(), true);
+            count++;
+            toCilent.println(""+count);
 
+        }catch(IOException e){
+
+        }
     }
 
     public static void main(String[] args) {
